b79c848b311919b647104ef8e0b9034419570c6d
Jhuguenin-code <jhuguenin@uchicago.edu>
2023-02-02 20:28:20 +0000

    Currency lists pages are working, actual exchange rate pages not included yet

---
+++ app/controllers/currency_controller.rb
@@ -0,0 +1,26 @@
+class CurrencyController<ApplicationController
+  def first_currency
+    @raw_data=open("https://api.exchangerate.host/symbols").read
+    @parsed_data=JSON.parse(@raw_data)
+    @symbols_hash=@parsed_data.fetch("symbols")
+
+
+      @array_of_symbols=@symbols_hash.keys
+      
+    render({:template=>"currency_templates/step1.html.erb"})
+  end
+
+  def second_currency
+    @raw_data=open("https://api.exchangerate.host/symbols").read
+    @parsed_data=JSON.parse(@raw_data)
+    @symbols_hash=@parsed_data.fetch("symbols")
+
+
+      @array_of_symbols=@symbols_hash.keys
+
+      #params hash {"from_currency"=>"ARS"}
+      @from_symbol= params.fetch("from_currency")
+      
+    render({:template=>"currency_templates/step2.html.erb"})
+  end
+end

91439b34c45ea918eb617f4458ec031b79b0e2fb
Jhuguenin-code <jhuguenin@uchicago.edu>
2023-02-02 21:50:13 +0000

    Basic infrastructure of exchange rate pages completed, unable to get ER data onto page

--- app/controllers/currency_controller.rb
+++ app/controllers/currency_controller.rb
@@ -1,5 +1,5 @@
 class CurrencyController<ApplicationController
-  def first_currency
+    def first_currency
     @raw_data=open("https://api.exchangerate.host/symbols").read
     @parsed_data=JSON.parse(@raw_data)
     @symbols_hash=@parsed_data.fetch("symbols")
@@ -22,5 +22,27 @@ class CurrencyController<ApplicationController
       @from_symbol= params.fetch("from_currency")
       
     render({:template=>"currency_templates/step2.html.erb"})
+  end
+
+  def third_currency
+    @raw_data=open("https://api.exchangerate.host/symbols").read
+    @parsed_data=JSON.parse(@raw_data)
+    @symbols_hash=@parsed_data.fetch("symbols")
+
+
+      @array_of_symbols=@symbols_hash.keys
+
+      
+
+    #opening exchange rate data
+    @er_data=open("https://api.exchangerate.host/convert").read
+    @parsed_er_data=JSON.parse(@er_data)
+    @er= @parsed_er_data.fetch("result")
+
+    #params hash
+    @from_symbol= params.fetch("from_currency")
+    @to_symbol= params.fetch("to_currency")
+      
+    render({:template=>"currency_templates/step3.html.erb"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get("/", {:controller=>"application", :action=>"homepage"})
   get("/forex", {:controller=>"currency", :action=>"first_currency"})
   get("/forex/:from_currency", {:controller=>"currency", :action=>"second_currency"})
+  get("/forex/:from_currency/:to_currency",{:controller=>"currency", :action=>"third_currency"} )
 end
